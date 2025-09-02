72cf15e2e068d40aed286d6205da43fe4bce8be7
MoonlightPanel <haminhchiennoi@gmail.com>
2025-09-02 10:41:29 +0700

    Create caidat.sh

---
+++ caidat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Code by NoiMC"
+apt-get update && apt-get install -y --no-install-recommends sudo wget python docker.io
+wget https://raw.githubusercontent.com/MoonlightPanel/IdlerVPsBot/refs/heads/main/Debian
+wget https://raw.githubusercontent.com/MoonlightPanel/IdlerVPsBot/refs/heads/main/Ubuntu
+docker build -t ubuntu-22.04-with-tmate -f Ubuntu . && docker build -t debian-with-tmate -f Debian .
+rm Ubuntu
+rm Debian
+wget https://raw.githubusercontent.com/MoonlightPanel/IdlerVPsBot/refs/heads/main/batdau.sh
+sh batdau.sh
