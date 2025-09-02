6b30eccb548d7d358bfaa486a959da5d46ad159c
MoonlightPanel <haminhchiennoi@gmail.com>
2025-09-02 11:05:03 +0700

    Update caidat.sh

--- caidat.sh
+++ caidat.sh
@@ -5,8 +5,6 @@ apt-get update && apt-get install -y --no-install-recommends sudo wget python do
 wget https://raw.githubusercontent.com/MoonlightPanel/IdlerVPsBot/refs/heads/main/Debian
 wget https://raw.githubusercontent.com/MoonlightPanel/IdlerVPsBot/refs/heads/main/Ubuntu
 docker build -t ubuntu-22.04-with-tmate -f Ubuntu . && docker build -t debian-with-tmate -f Debian .
-rm Ubuntu
-rm Debian
 wget https://raw.githubusercontent.com/MoonlightPanel/IdlerVPsBot/refs/heads/main/batdau.sh
 wget https://raw.githubusercontent.com/MoonlightPanel/IdlerVPsBot/refs/heads/main/batdau.py
 echo "Token Discord Bot:"
